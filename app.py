7e6c5d556745ec043ca7a1a57e3f9fa21978a491
mochi-w650z <mochice.mls.ntl@gmail.com>
2021-07-01 20:54:04 +0900

    add sample routing

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/start/<user_id>", methods="POST")
+def start(user_id):
+    pass
+
+
 if __name__ == "__main__":
     app.run(debug=True)
