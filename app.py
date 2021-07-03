edf0fdd4e4e486c761c936362471c0e6ba4ba4f0
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-07-03 13:24:03 +0900

    add sample routing

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from datetime import date
+
+from flask import Flask, jsonify, request
+
+import db
 
 app = Flask(__name__)
 
 
 @app.route("/start/<user_id>", methods="POST")
-def start(user_id):
+def store(user_id):
+    post_data = request.get_json()
+    today = date.today()
+    db.update(post_data, day=today)
+
+
+@app.route("/api/<user_id>", methods="GET")
+def get_recent_week_data(user_id):
     pass
 
+@app.route("/api/<user_id>/<language>", methods="GET")
+def get_recent_week_data_with_language(user_id, language): 
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
