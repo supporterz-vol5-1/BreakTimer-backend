fd11f15a1295b0590ac4c7aa3defd83376b7243c
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-09-25 17:42:24 +0900

    :+1: Add sample sql for execute test

---
+++ sample.sql
@@ -0,0 +1,18 @@
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'typescript', 400.50, '2021-06-27' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'html', 3600, '2021-06-27' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'css', 1234.56, '2021-06-27' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'python', 802.11, '2021-06-28' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'cpp', 100.300, '2021-06-28' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'typescript', 600.56, '2021-06-29' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'python', 2000, '2021-06-29' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'go', 555.55, '2021-06-29' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'html', 1000.20, '2021-06-30' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'typescript', 623.11, '2021-07-01' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5 -1', 'python', 3051.00, '2021-07-01' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'ruby', 973.22, '2021-07-02' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'python', 88823.00, '2021-07-02' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'cobol', 1.00, '2021-07-03' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'typescript', 888.20, '2021-07-03' ) ;
+insert into work_times( user_name, filetype, work_time, day ) values( 'hackathon-vol5-1', 'python', 3000.11, '2021-07-03' ) ;
+insert into users values( 'hackathon-vol5-1', '56af743f9ff7a944bc57f26bb9b1605b' ) ;
+insert into users values( 'non-active',       'thisissamplethisissamplethisissa' ) ;
