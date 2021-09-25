b3dbaec3fee7fa2470f2368f0df5c24ad5d00aac
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-09-25 15:11:37 +0900

    :memo: Refactor test

--- tests/test_app.py
+++ tests/test_app.py
@@ -38,33 +38,41 @@ def client():
 
 
 def test_get_root(client):
+    """User should not access root
+    """
+
     r = client.get("/")
     assert r.status_code == 404
 
 
 def test_api_root(client):
+    """User should not access api without username
+    """
     r = client.get("/api")
     assert r.status_code == 404
 
 
-def test_post_data(client):
+def test_post_data_without_body(client):
+    """User should not post without body
+    """
     r = client.post("/api/hackathon-vol5-1")
     assert r.status_code == 403
 
+
+def test_post_data_must_be_json(client):
+    """Post data must be json
+    """
     r = client.post(
-        "/api/hackathon-vol5-1", data={"body": {"filetype": "python", "work_time": 300}}
+        "/api/hackathon-vol5-1",
+        data={
+            "body": {
+                "filetype": "python",
+                "work_time": 300,
+                "token": "56af743f9ff7a944bc57f26bb9b1605b",
+            }
+        },
     )
     assert r.status_code == 403
-
-    r = client.post(
-        "/api/hackathon-vol5-1", json={"body": {"filetype": "python", "work_time": 300}}
-    )
-    assert r.status_code == 403
-
-    # r = client.post(
-    #     "/api/invalid-user", json={"body": {"filetype": "python", "work_time": 300}}
-    # )
-    # assert r.status_code == 404
 
     r = client.post(
         "/api/hackathon-vol5-1",
@@ -79,20 +87,61 @@ def test_post_data(client):
     assert r.status_code == 200
 
 
+def test_post_data_without_token(client):
+    """User should not post without token
+    """
+    r = client.post("/api/hackathon-vol5-1",
+                    data={"body": {
+                        "filetype": "python",
+                        "work_time": 300
+                    }})
+    assert r.status_code == 403
+
+    r = client.post("/api/hackathon-vol5-1",
+                    json={"body": {
+                        "filetype": "python",
+                        "work_time": 300
+                    }})
+    assert r.status_code == 403
+
+    # r = client.post(
+    #     "/api/invalid-user", json={"body": {"filetype": "python", "work_time": 300}}
+    # )
+    # assert r.status_code == 404
+
+
+@pytest.mark.freeze_time("2021-07-04 12:00:00")
 def test_get_user_data(client):
+    """User can get information via GET method
+    """
     r = client.get("/api/hackathon-vol5-1")
     assert r.status_code == 200
 
     d = r.json
     assert len(d) == 7
 
+    for dd in d:
+        assert bool(dd)    # this user is fully active
+
 
 def test_get_invalid_user_data(client):
+    """User should not access unexisted user
+    """
     r = client.get("/api/invalid-user")
     assert r.status_code == 404
 
 
+def test_get_non_active_user_info(client):
+    """User can get non active user's information
+    """
+    r = client.get("/api/non-active")
+    for d in r.json:
+        assert not bool(d)    # if dict is empty, bool({}) will is False.
+
+
 def test_get_user_data_with_filetype(client):
+    """User can get information with specifying language
+    """
     r = client.get("/api/hackathon-vol5-1/python")
     assert r.status_code == 200
 
